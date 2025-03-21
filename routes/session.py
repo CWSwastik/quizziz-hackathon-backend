22cf9c78aaa18c312c33fe3a91718702948c7b1f
Swastik Goswami <110041714+CWSwastik@users.noreply.github.com>
2025-03-21 14:51:09 +0530

    Update session.py

--- routes/session.py
+++ routes/session.py
@@ -3,12 +3,14 @@ from models.schemas import (
     GenerateSessionRequest,
     GenerateSessionResponse,
     Dialogue,
-    QuizQuestion, GenerateVoiceRequest
+    QuizQuestion,
+    GenerateVoiceRequest,
 )
+from models.schemas import SummarizePDFResponse
+
 from services.script_generator import generate_script
 from services.quiz_generator import generate_quiz
 from services.pdf_parser import extract_text_from_pdf, summarize_text
-from models.schemas import SummarizePDFResponse
 from services.voice_generator import generate_speech_stream
 from fastapi.responses import StreamingResponse
 
@@ -22,8 +24,8 @@ async def generate_learning_session(request: GenerateSessionRequest):
     )
 
     final_script = []
-    for speaker, msg in script:
-        final_script.append(Dialogue(speaker=speaker, dialogue=msg))
+    for speaker, pose, msg in script:
+        final_script.append(Dialogue(speaker=speaker, pose=pose, dialogue=msg))
 
     quiz = generate_quiz(request.topic_prompt)
     final_quiz = []
@@ -38,6 +40,7 @@ async def generate_learning_session(request: GenerateSessionRequest):
 
     return {"script": final_script, "quiz": final_quiz}
 
+
 @router.post("/upload-pdf/", response_model=SummarizePDFResponse)
 async def upload_pdf(file: UploadFile = File(...)):
     """Endpoint to upload a PDF and return its summarized content."""
@@ -47,7 +50,9 @@ async def upload_pdf(file: UploadFile = File(...)):
 
 
 @router.get("/generate-voice/")
-async def generate_voice_endpoint(text: str = Query(..., description="Text to convert to speech")):
+async def generate_voice_endpoint(
+    text: str = Query(..., description="Text to convert to speech")
+):
     """
     Streams AI-generated speech using ElevenLabs API.
     """
