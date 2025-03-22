6774762d60edbd9be79f5a6d0babebf6a685c545
Swastik Goswami <cwswastik2005@gmail.com>
2025-03-22 10:47:22 +0530

    fix: cors

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
 from routes import session, characters
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="PersonaEd API")
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 # Include routers
 app.include_router(session.router, prefix="/session", tags=["Session"])
 # app.include_router(pdf.router, prefix="/pdf", tags=["PDF"])
